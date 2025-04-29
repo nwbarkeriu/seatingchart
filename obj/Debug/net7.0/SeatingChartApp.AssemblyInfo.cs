4d4b9cfef2afd5386594c13a47301cc16a59a43b
Barker <Nathan.Barker@wolterskluwer.com>
2025-04-29 14:11:13 -0400

    Initial clean commit

--- obj/Debug/net7.0/SeatingChartApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/SeatingChartApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeatingChartApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc9bd54f1e483aa79a9094fda47dce98d1ccc28")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeatingChartApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeatingChartApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
