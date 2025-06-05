4cde7d2b218a97103f135d881b8c50e446623c9c
Barker <Nathan.Barker@wolterskluwer.com>
2025-06-05 08:33:12 -0400

    Added Lunch API controller and updated nav without Volleyball page

--- Program.cs
+++ Program.cs
@@ -18,14 +18,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
-
+app.MapControllers();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-
 app.Run();
